8de50ba39f76553feed4a24408a8e45bf910ce34
RMike1 <71224316+RMike1@users.noreply.github.com>
2024-10-27 13:36:38 +0200

    adding vue..

--- routes/api.php
+++ routes/api.php
@@ -5,23 +5,32 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\AuthController;
 
-Route::get('all-employess',[AuthController::class,'all_employess'])->middleware(['auth:sanctum']);
 
 Route::post('login',[AuthController::class,'login'])->name('login');
 
-Route::post('logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
 
-Route::post('add-employee',[AuthController::class,'add_employee'])->middleware(['auth:sanctum']);
+Route::middleware(['auth:sanctum'])->group(function(){
+    
+    Route::post('logout',[AuthController::class,'logout']);
+    
+    Route::get('all-employess',[AuthController::class,'all_employess']);
+    
+    Route::post('add-employee',[AuthController::class,'add_employee']);
+    
+    Route::put('update-employee/{user}',[AuthController::class,'update_employee']);
+    
+    Route::delete('delete-employee/{user}',[AuthController::class,'delete_employee']);
+    
+    Route::post('clock-in',[HomeController::class,'clockIn']);
+    
+    Route::post('clock-out',[HomeController::class,'clockOut']);
+    
+    Route::get('attendance',[HomeController::class,'attendance']);
 
-Route::put('update-employee/{user}',[AuthController::class,'update_employee'])->middleware(['auth:sanctum']);
+});
 
-Route::delete('delete-employee/{user}',[AuthController::class,'delete_employee'])->middleware(['auth:sanctum']);
 
-Route::post('clock-in',[HomeController::class,'clockIn'])->middleware(['auth:sanctum']);
 
-Route::post('clock-out',[HomeController::class,'clockOut'])->middleware(['auth:sanctum']);
-
-Route::get('attendance',[HomeController::class,'attendance'])->middleware(['auth:sanctum']);
 
 
 
